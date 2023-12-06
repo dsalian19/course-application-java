c36211edbe9684517d9f3144c73d841193b332c4
deepaksalian <dsali5@illinois.edu>
2023-12-06 00:10:31 -0600

    finished project

--- app/src/main/java/edu/illinois/cs/cs124/ay2023/mp/activities/CourseActivity.java
+++ app/src/main/java/edu/illinois/cs/cs124/ay2023/mp/activities/CourseActivity.java
@@ -1,6 +1,7 @@
 package edu.illinois.cs.cs124.ay2023.mp.activities;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,20 +12,26 @@ import edu.illinois.cs.cs124.ay2023.mp.application.CourseableApplication;
 import edu.illinois.cs.cs124.ay2023.mp.helpers.ResultMightThrow;
 import edu.illinois.cs.cs124.ay2023.mp.models.Summary;
 import edu.illinois.cs.cs124.ay2023.mp.models.Course;
+import edu.illinois.cs.cs124.ay2023.mp.models.Rating;
 import java.util.function.Consumer;
 
-public class CourseActivity extends AppCompatActivity {
+public class CourseActivity extends AppCompatActivity
+    implements RatingBar.OnRatingBarChangeListener {
   private Course course;
+  private Rating rating;
   private Summary summary;
   @Override
   protected void onCreate(@Nullable Bundle unused) {
     super.onCreate(unused);
     setContentView(R.layout.activity_course);
+    RatingBar ratingView = findViewById(R.id.rating);
+    ratingView.setOnRatingBarChangeListener(this);
     try {
       ObjectMapper objectMapper = new ObjectMapper();
       summary = objectMapper.readValue(getIntent().getStringExtra("summary"), Summary.class);
       CourseableApplication application = (CourseableApplication) getApplication();
       application.getClient().getCourse(summary, courseCallback);
+      application.getClient().getRating(summary, ratingCallback);
     } catch (JsonProcessingException e) {
       throw new RuntimeException();
     }
@@ -36,5 +43,23 @@ public class CourseActivity extends AppCompatActivity {
       descriptionTextView.setText(summary.toString() + " " + course.getDescription());
     });
   };
-
+  private final Consumer<ResultMightThrow<Rating>> ratingCallback =
+      (result) -> {
+        try {
+          rating = result.getValue();
+          runOnUiThread(() -> {
+            RatingBar ratingBar = findViewById(R.id.rating);
+            ratingBar.setRating(rating.getRating());
+          });
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      };
+  @Override
+  public void onRatingChanged(RatingBar ratingBar, float ratingValue, boolean fromUser) {
+    CourseableApplication app = (CourseableApplication) getApplication();
+    Rating newRating = new Rating(rating.getSummary(), ratingValue);
+    app.getClient().postRating(newRating, ratingCallback);
+    app.getClient().getRating(summary, ratingCallback);
+  }
 }
